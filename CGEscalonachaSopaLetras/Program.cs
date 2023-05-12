47dc4a2f2aca098a64af82f3386f9a19ef475b2b
Cecilia Gabriela Escalona Chavarria <cescalonacha@gmail.com>
2023-05-12 10:54:14 -0600

    Add project files.

---
+++ CGEscalonachaSopaLetras/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGEscalonachaSopaLetras
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SopaLetras sopaLetras = new SopaLetras();
+           
+           
+          
+            bool salir = false;
+            while (!salir)
+            {
+                Console.WriteLine("Bienvenido al Menu");
+                Console.WriteLine("1. OrdenAlfabetico 1 ");
+                Console.WriteLine("2. SimboloSimple 2 ");
+            
+                Console.WriteLine("5. Salir");
+
+                int opcion = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine();
+                        sopaLetras.OrdenAlfabetico();
+
+
+                        break;
+                    case 2:
+
+                        Console.WriteLine();
+                     
+
+                        break;
+                  
+
+
+                    case 5:
+                        Console.WriteLine("Has elegido salir de la aplicación");
+                        salir = true;
+                        break;
+                    default:
+                        Console.WriteLine("Elige una opcion entre 1 y 4");
+                        break;
+                }
+
+            }
+        }
+}
+    }
---
+++ CGEscalonachaSopaLetras/SopaLetras.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGEscalonachaSopaLetras
+{
+    internal class SopaLetras
+    {
+        string palabra;
+        public void OrdenAlfabetico()
+        {
+            Console.WriteLine("Escriba la Palabra");
+            string palabra = Console.ReadLine();
+
+            string orden = new string(palabra.OrderBy(x => x).ToArray());
+            Console.WriteLine(orden);
+            Console.ReadKey();
+            Console.ReadLine();
+        }
+    }
+}
